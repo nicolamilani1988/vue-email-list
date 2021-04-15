31c0c93fdea4fc1c701618326fb8a7af7af123b5
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-15 21:57:14 +0200

    stampate mail con axios

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  CHIAMATA CON JQUERY E AJAX
 function printMail(val){
     for(let i=0;i<val;i++){
         mailGenerator();
@@ -8,9 +9,9 @@ function mailGenerator(){
     $.ajax({
         url: 'https://flynn.boolean.careers/exercises/api/random/mail',
         method: 'GET',
-        success: function(data){           
+        success: function(data){
                 const res = data['response'];
-                $("#emails").append(`<div>${res}</div>`);           
+                $("#email-list").append(`<li>${res}</li>`);
         },
         error: function(){
             console.log("ERRORE");
@@ -22,4 +23,25 @@ function init(){
     printMail(10);
 }
 
+// CHIAMATA CON VUE E AXIOS
+function initVue(){
+    new Vue ({
+        el: "#app",
+        data: {
+            emails: [],
+        },
+        
+        mounted(){
+            for( let i = 0; i<10; i++){
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => ( this.emails.push(response['data']['response'])));               
+               
+            }
+        },
+    });    
+    
+}
+
 document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded',initVue);
